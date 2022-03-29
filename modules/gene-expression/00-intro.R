e7fec86a7adee2a5897904a3f03f7fd674f158c4
rintukutum <rintukutum@gmail.com>
2022-03-29 14:27:41 +0530

    updated

--- modules/gene-expression/00-intro.R
+++ modules/gene-expression/00-intro.R
@@ -11,6 +11,7 @@ plot(num_vec)
  plot(rev_vec)
 char_vec [1] <- 'b'
 char_vec [2] <- 4
+char_vec [3] <- 'c'
 char_vec
 rm(list = ls())
 ls()
@@ -18,15 +19,51 @@ ls()
 #-------------- R Objects {Vector}
 x_num <- c(0.5, 0.6)       ## numeric
 x_logic <- c(TRUE, FALSE)    ## logical
+x_logic
 x_logic <- c(T, F)           ## logical
+T <- 'x'
+T
 x_char <- c("a", "b", "c")  ## character
 x_integer <- 9:29              ## integer
 x_complex <- c(1+0i, 2+4i)     ## complex
 
 #-------------- Mix objects
 y_char <- c(1.7, "a")   ## character
-y_num <- c(TRUE, 2)    ## numeric
+y_char
+y_num <- c(TRUE, FALSE, 2)    ## numeric
+y_num
 y_char <- c("a", TRUE)  ## character
+y_char
+
+#---------------Matrix
+
+matrix()
+help(matrix)
+set.seed(121)
+rand_vec <- sample (1:10, 1000, replace = TRUE)
+
+mat_demo <-matrix(data = rand_vec,  nrow = 100, ncol = 10, byrow = FALSE,
+       dimnames = NULL)
+mat_demo
+
+mean (rand_vec)
+median(rand_vec)
+mean(mat_demo)
+view(mat_demo)
+mean(mat_demo[,1])
+apply(X = mat_demo, MARGIN = 1, FUN = mean)
+apply(X = mat_demo, MARGIN = 1, FUN = min)
+
+apply(mat_demo[, 1:5], 1,mean)
+apply(mat_demo[, 6:10], 1,mean)
+num_demo <- c(3,2,4,5)
+idx_demo <- num_demo >2
+num_demo[idx_demo]
+idx_demo_eq <- num_demo ==2
+idx_demo_eq
+num_demo [idx_demo_eq]
+heatmap(mat_demo)
+
 
 #-------------- Explicit Coercion Objects
 x <- 0:6
